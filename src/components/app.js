7ffda077b0e9d96c0405c2e054cb8bc918ac7e90
Maximilian Menschel <elmaxo@hotmail.de>
2017-10-02 16:00:16 -0500

    Initial Commit -Setup React project with web pack dev server

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("Hallo App")
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+      <h1>TRUE VOCAB TRAINER</h1>
+      </div>
+    );
+  }
+}
+export default App;
